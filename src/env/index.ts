c49c2fed3e3a6da9de82b25c953cb94c94cd76db
alessandro.uleon1 <alessandro.uleon@grupoicts.com.br>
2024-01-24 17:21:31 -0400

    feat: implementation in roots

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   DATABASE_URL: z.string(),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  PORT: z.number().default(3333),
+  PORT: z.number().default(3334),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,13 @@
 import fastify from 'fastify'
-import crypton from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/help', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypton.randomUUID(),
-      title: 'Transaction de teste',
-      amount: 1000,
-      session_id: crypton.randomUUID(),
-    })
-    .returning('*')
-  return transaction
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
